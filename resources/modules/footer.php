30c41be734929d0a80d6cd07b5fdaf982022072a
dannycho7 <dannycho7@gmail.com>
2017-07-06 21:39:47 -0700

    No longer supporting old php module structure

--- resources/modules/footer.php
+++
@@ -1,9 +0,0 @@
-</body>
-<script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script><script src = 'js/web.js'></script>
-<script src = 'js/index.js'></script>
-<script src = 'js/photo.js'></script>
-<script type="text/javascript" src="js/projects.js"></script>
-</html>
