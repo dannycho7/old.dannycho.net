606aa2ed82b8a315ec5380c706e082256fb8e845
dannycho7 <dannycho7@gmail.com>
2017-07-06 21:37:58 -0700

    Gulpfile: Add concat js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass');
- 
+var concat = require('gulp-concat');
+
 gulp.task('sass', function () {
 	return gulp.src('./resources/assets/sass/theme.scss')
 			.pipe(sass().on('error', sass.logError))
@@ -9,4 +10,10 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
 	gulp.watch('./resources/assets/sass/theme.scss', ['sass']);
+});
+
+gulp.task('concat', function() {
+	gulp.src('./resources/assets/js/*.js')
+	.pipe(concat('build.js'))
+	.pipe(gulp.dest('public_html/js'))
 });
