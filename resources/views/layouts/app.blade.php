8a87c6e534fbacd718b9d354b6b4efaed2f8b32b
dannycho7 <dannycho7@gmail.com>
2017-07-07 23:49:57 -0700

    Edit responsiveness

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:title" content="Danny's Personal Website">
     <meta property="og:type" content="Personal.Website">
     <meta property="og:url" content="http://dannycho.net">
