09418c70a7aa7a262200092f24ba505efc77b123
dannycho7 <dannycho7@gmail.com>
2017-07-08 19:14:15 -0700

    Modify intro description

--- resources/views/partials/index/intro_descr.blade.php
+++ resources/views/partials/index/intro_descr.blade.php
@@ -1,1 +1,8 @@
-<p>I am <strong>Danny Cho</strong>, a <strong>web</strong> developer</strong> &amp; <strong>entrepreneur</strong>. I have been <strong>coding</strong> &amp; attending <strong>hackathons</strong> for 2 years. I am currently a <strong>Computer Science</strong> major at <strong>UC Santa Barbara</strong>. I always look forward to meeting new people and collaborating on <strong>projects</strong>. Feel free to <a href='mailto:dannycho7@gmail.com' target="_blank">contact me</a> for anything! This site was made by Danny Cho using Laravel and PHP</p>
+<p>
+	Hello! My name is <strong>Danny Cho</strong>, and I like <strong>building</strong> things.
+	I have been <strong>coding</strong> &amp; attending <strong>hackathons</strong> for 2 years.
+	I am currently a <strong>Computer Science</strong> major at <strong>UC Santa Barbara</strong>.
+	I always look forward to meeting new people and collaborating on <strong>projects</strong>.
+	If you'd like a copy of my resume, please send me a request via email <a href='mailto:dannycho7@gmail.com?Subject=Resume' target="_blank">here</a>.
+	This site was made by Danny Cho using Laravel and PHP.
+</p>
