af1368779ccc46bb68320fd67ef033b9bece45a9
dannycho7 <dannycho7@gmail.com>
2017-10-22 10:45:18 -0700

    Add Hackathon wins to intro description

--- resources/views/partials/index/intro_descr.blade.php
+++ resources/views/partials/index/intro_descr.blade.php
@@ -3,6 +3,7 @@
 	I study <strong>Computer Science</strong> at <strong>UC Santa Barbara</strong>.
 	I worked as software engineer intern at <strong>Appfolio</strong> during Summer 2017. Previously, I was a software engineer intern at <strong>SmartRG Inc</strong>.
 	I'm the co-director/developer team lead at <a href="http://sbhacks.com">SB Hacks</a>.
+	I also attend hackathons as often as I can and have won/placed in <strong>seven</strong> different Hackathons.
 	I always look forward to meeting new people and collaborating on <strong>projects</strong>.
 	If you'd like a copy of my resume, please send me a request via email <a href='mailto:dannycho7@gmail.com?Subject=Resume' target="_blank">here</a>.
 	This site was made by Danny Cho using Laravel and PHP.
