b1a5dba9bc17bd022bfff97ac09d0f0e4b367dda
dannycho7 <dannycho7@gmail.com>
2017-11-24 17:31:24 -0800

    Add new hello@dannycho.net email to intro description

--- resources/views/partials/index/intro_descr.blade.php
+++ resources/views/partials/index/intro_descr.blade.php
@@ -5,6 +5,6 @@
 	I'm the co-director/developer team lead at <a href="http://sbhacks.com">SB Hacks</a>.
 	I also attend hackathons as often as I can and have won/placed in <strong>seven</strong> different Hackathons.
 	I always look forward to meeting new people and collaborating on <strong>projects</strong>.
-	If you'd like a copy of my resume, please send me a request via email <a href='mailto:dannycho7@gmail.com?Subject=Resume' target="_blank">here</a>.
+	If you'd like a copy of my resume, or have any questions please reach out to me via email at <a href='mailto:hello@dannycho.net?Subject=Resume'>hello@dannycho.net</a>.
 	This site was made by Danny Cho using Laravel and PHP.
 </p>
